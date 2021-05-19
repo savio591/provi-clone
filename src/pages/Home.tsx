2cbbf286483a94cc40d638533a6b91832aecb531
savio591 <savio591@hotmail.com>
2021-05-19 11:43:34 -0300

    Page Home: added Header Component and Burguer Menu Modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,25 @@
+import { GlobalStyle } from "../styles/global";
+
+import { Header } from "../components/Header";
+import { MobileMenuModal } from "../components/Header/MobileMenuModal";
+import { useState } from "react";
+
 export function Home() {
-  return <h1>Hello World!</h1>
+  const [isMenuModalActive, setIsMenuModalActive]  = useState(false)
+
+  function handleOpenMenuModal() {
+    setIsMenuModalActive(true);
+  }
+
+  function handleCloseMenuModal() {
+    setIsMenuModalActive(false);
+  }
+
+  return (
+    <>
+      <Header onHandleBurguerButton={handleOpenMenuModal} />
+      <MobileMenuModal isOpen={isMenuModalActive} onRequestClose={handleCloseMenuModal}  />
+      <GlobalStyle />
+    </>
+  );
 }

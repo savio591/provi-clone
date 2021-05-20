553c3d6c60907eb9b44ce4242c01821172628d0e
savio591 <savio591@hotmail.com>
2021-05-20 18:03:59 -0300

    Keen-slider styling system #23 | Global styled fonts #24

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -50,11 +50,109 @@ button {
 
 img {
   display: block;
+
+  &.rotate180 {
+    transform: rotate(180deg);
+  }
 }
+
+button.clean {
+  border: none;
+  background: transparent;
+}
+
 
 [disabled] {
     opacity: 0.6;
     cursor: default
+}
+
+h1.text60 {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 60px;
+  line-height: 74px;
+
+  color: #FFFFFF;
+}
+
+h1.text48 {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 48px;
+  line-height: 60px;
+
+  color: #FFFFFF;
+}
+
+h1.text38, h2.text38 {
+  font-size: 38px;
+  line-height: 47px;
+
+  color: #FFFFFF;
+}
+
+h1.text30 {
+  font-size: 30px;
+line-height: 36px;
+
+color: #ffffff
+}
+
+h2.text24, p.text24 {
+font-size: 1.5rem;
+line-height: 30px;
+
+color: #FFFFFF;
+}
+
+p.text24_38 {
+  font-size: 1.5rem;
+line-height: 38px;
+
+color: #FFFFFF;
+}
+
+
+span {
+    font-weight: bold;
+  }
+
+p.text20 {
+font-size: 20px;
+line-height: 25px;
+text-align: center;
+
+color: #FFFFFF;
+
+}
+
+a.textLink16 {
+font-style: normal;
+font-weight: bold;
+font-size: 1rem;
+line-height: 1.25rem;
+display: flex;
+align-items: center;
+text-decoration-line: underline;
+
+color: #FFFFFF;
+}
+
+h1.blue, h2.blue, h3.blue, p.blue, a.blue {
+  color: var(--blue);
+}
+
+h1.magenta, h2.magenta, h3.magenta, p.magenta, a.magenta {
+  color: var(--magenta);
+}
+
+h1.center, p.center, h2.center {
+  text-align: center;
+}
+
+h1.left, h2.left, p.left{
+  text-align: left;
 }
 
 .react-modal-overlay {
@@ -88,5 +186,38 @@ img {
     filter: brightness(0.8)
   }
 }
+
+
+.keen-slider {
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  user-select: none;
+  -webkit-touch-callout: none;
+  -khtml-user-select: none;
+  -ms-touch-action: pan-y;
+  touch-action: pan-y;
+  -webkit-tap-highlight-color: transparent;
+
+  &__slide {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    min-height: 100%;
+  }
+
+  &[data-keen-slider-v] {
+    flex-wrap: wrap;
+  }
+
+  &[data-keen-slider-v] &__slide {
+    width: 100%;
+  }
+
+  &[data-keen-slider-moves] * {
+    pointer-events: none;
+  }
+}
+
 
 `;

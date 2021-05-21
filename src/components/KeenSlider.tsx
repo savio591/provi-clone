7aee07d1b490422eeabfa1d464d4aff658919275
savio591 <savio591@hotmail.com>
2021-05-21 14:23:06 -0300

    Component: Keen-slider #25

---
+++ src/components/KeenSlider.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, RefObject } from "react";
+
+interface KeenSliderProps {
+  content: ReactNode[];
+  sliderRef: RefObject<HTMLDivElement>;
+}
+
+export function KeenSlider({ content, sliderRef }: KeenSliderProps) {
+  return (
+    <div ref={sliderRef} className="keen-slider cards">
+      {content.map((item, id) => {
+        return (
+          <div key={id} className="keen-slider__slide">
+            {item}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
